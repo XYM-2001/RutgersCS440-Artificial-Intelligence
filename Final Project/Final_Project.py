618b9c0b8ebe82727ccc24f9219ec8626556fa2e
XYM-2001 <xmeng975@gmail.com>
2023-04-22 19:59:49 -0400

    updated perceptron model

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,39 +1,56 @@
 import os
 import numpy as np
 
-class Perceptron:
-    def __init__(self, learning_rate=0.01, n_iterations=1000):
-        self.learning_rate = learning_rate
-        self.n_iterations = n_iterations
-        self.weights = None
-        self.bias = None
-        
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
-        
-        # Initialize weights and bias
-        self.weights = np.zeros(n_features)
-        self.bias = 0
-        
-        # Update weights and bias using the perceptron learning rule
-        for _ in range(self.n_iterations):
-            for i in range(n_samples):
-                linear_output = np.dot(self.weights, X[i]) + self.bias
-                y_pred = np.where(linear_output >= 0, 1, -1)
-                
-                # Update weights and bias if prediction is incorrect
-                if y_pred != y[i]:
-                    self.weights += self.learning_rate * y[i] * X[i]
-                    self.bias += self.learning_rate * y[i]
-    
-    def predict(self, X):
-        linear_output = np.dot(X, self.weights) + self.bias
-        y_pred = np.where(linear_output >= 0, 1, -1)
-        return y_pred
-
 def display_image(image):
     for i in image:
         print(i)
+
+class Perceptron:
+    def __init__(self, num_features):
+        # Initialize the weights and bias to small random values
+        self.weights = np.random.randn(num_features)
+        self.bias = np.random.randn()
+
+    def predict(self, features):
+        # Calculate the weighted sum of the features
+        weighted_sum = np.dot(features, self.weights) + self.bias
+
+        # Apply the step function to the weighted sum
+        if weighted_sum >= 0:
+            return 1
+        else:
+            return 0
+        
+# class Perceptron:
+#     def __init__(self, lr=0.1, epochs=100):
+#         self.lr = lr
+#         self.epochs = epochs
+
+#     def fit(self, X, y):
+#         # Add a bias term to the input data
+#         X = np.hstack((X, np.ones((X.shape[0], 1))))
+
+#         # Initialize weights to zeros
+#         self.weights = np.zeros(X.shape[1])
+
+#         # Train the perceptron
+#         for epoch in range(self.epochs):
+#             for i in range(X.shape[0]):
+#                 if y[i] * np.dot(self.weights, X[i]) <= 0:
+#                     self.weights += self.lr * y[i] * X[i]
+
+#     def predict(self, X):
+#         # Add a bias term to the input data
+#         X = np.hstack((X, np.ones((X.shape[0], 1))))
+
+#         # Compute the dot product of the input data and the weights
+#         dot_product = np.dot(X, self.weights)
+
+#         # Compute the predicted class labels as 0 or 1
+#         y_pred = np.where(dot_product > 0, 1, 0)
+
+#         return y_pred
+    
 def load_imagelabel(filename):
     __location__ = os.path.dirname(__file__)
     f = open(os.path.join(__location__, 'data\\digitdata\\' + filename))
@@ -50,7 +67,7 @@ def load_image(filename, number):
     for i in range(number):
         data = []
         for j in range(28):
-            data.append(list(temp.pop())[0:27])
+            data.append(list(temp.pop())[0:28])
             if not temp:
                 print('Reached the end of file.')
                 items.append(data)
@@ -62,15 +79,39 @@ def load_image(filename, number):
     f.close()
     return items
 
-# def convert_Integer(image):
-#     converted = []
-#     for row in image:
-#         for col in row:
+def convert_Integer(image):
+    converted = []
+    for row in image:
+        converted_row = []
+        for pixel in row:
+            if pixel == '+':
+                converted_row.append(1)
+            elif pixel == '#':
+                converted_row.append(2)
+            else:
+                converted_row.append(0)
+        converted.append(converted_row)
+    return np.array(converted)
 
+def train_model(training_set, training_label, perceptron):
+    for i in range(len(training_set)):
+        features = convert_Integer(training_set[i]).flatten()/255.0
+        prediction = perceptron.predict(features)
+        error = np.abs(training_label[i] - prediction)
+        perceptron.weights += error*training_set[i]
+        perceptron.bias += error
+    return perceptron
 
 def main():
     labels = load_imagelabel('traininglabels')
     images = load_image('trainingimages', len(labels))
+    perceptron = Perceptron(num_features=28*28)
+
+    converted = convert_Integer(images[1])
+    display_image(converted)
+    features = converted.flatten() / 255.0
+    perceptron = Perceptron(num_features=len(features))
+    
 
 if __name__=="__main__":
     main()
--- Final Project/test.py
+++ Final Project/test.py
@@ -1,16 +1,78 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_digits
 
-digits = load_digits()
-images = digits.images
-targets = digits.target
+# Define the image as a list of strings
+image = [
+    "        ",
+    "        ",
+    "  ++#++ ",
+    " +##### ",
+    "+######+",
+    "+#####+##",
+    "+######++#+",
+    "+###++##++#+",
+    "++###+     ",
+    "+##+ +     ",
+    "+##+       ",
+    "+##+       ",
+    "+##+       ",
+    "++#+       ",
+    "+##+       ",
+    "+##+       ",
+    "+##+       ",
+    "+##+++++###++",
+    "+#########+",
+    "+#######+",
+    " ++###++",
+    "        ",
+    "        ",
+]
 
-# plot the first 5 images
-fig, axes = plt.subplots(nrows=1, ncols=5, figsize=(10, 3))
-for i, ax in enumerate(axes):
-    ax.imshow(images[i], cmap='gray')
-    ax.set_title(f"Label: {targets[i]}")
-    ax.axis('off')
-    
-plt.show()
+# Convert the list of strings to a 2D numpy array of integers
+digits = np.array([[1 if c == '+' or c == '#' else 0 for c in row] for row in image])
+
+# Define the perceptron class
+class Perceptron:
+    def __init__(self, lr=0.1, epochs=100):
+        self.lr = lr
+        self.epochs = epochs
+
+    def fit(self, X, y):
+        # Add a bias term to the input data
+        X = np.hstack((X, np.ones((X.shape[0], 1))))
+
+        # Initialize weights to zeros
+        self.weights = np.zeros(X.shape[1])
+
+        # Train the perceptron
+        for epoch in range(self.epochs):
+            for i in range(X.shape[0]):
+                if y[i] * np.dot(self.weights, X[i]) <= 0:
+                    self.weights += self.lr * y[i] * X[i]
+
+    def predict(self, X):
+        # Add a bias term to the input data
+        X = np.hstack((X, np.ones((X.shape[0], 1))))
+
+        # Compute the dot product of the input data and the weights
+        dot_product = np.dot(X, self.weights)
+
+        # Compute the predicted class labels as 0 or 1
+        y_pred = np.where(dot_product > 0, 1, 0)
+
+        return y_pred
+
+# Define the training data and labels
+X_train = digits.reshape(1, -1)
+print(digits)
+print(X_train)
+# y_train = np.array([0])
+
+# # Create a perceptron object and fit the training data
+# perceptron = Perceptron()
+# perceptron.fit(X_train, y_train)
+
+# # Predict the label of the digit image
+# y_pred = perceptron.predict(X_train)
+
+# # Print the predicted label
+# print("Predicted label:", y_pred[0])
